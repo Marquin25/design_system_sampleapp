047979df97eba8cdaf5bfa7bba81daad5db52379
Marcus Antônio Rodrigues Monteiro Rios de Pina <114959566+Marquin25@users.noreply.github.com>
2025-10-16 17:32:04 -0300

    Delete ds_mobileii_project directory

--- ds_mobileii_project/lib/components/ds_tabbar_delegate.dart
+++
@@ -1,5 +0,0 @@
-// CLASSE ABSTRATA QUE DEFINE A ASSINATURA DO METODO 
-// PARA TRATAR O EVENTO DO COMPONENTE TABBAR
-abstract class DSTabBarDelegate {
-  void onTabSelected(int index, String tabTitle);
-}
